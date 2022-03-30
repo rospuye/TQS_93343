d2df2266c2a6de49ce4acfc4dcfa508a43bcf665
rospuye <66674322+rospuye@users.noreply.github.com>
2022-03-30 20:45:46 +0100

    lab3 complete

--- lab3/lab3_2carsService/src/test/java/tqs/lab3_2carsService/D_IntegrationTest.java
+++ lab3/lab3_2carsService/src/test/java/tqs/lab3_2carsService/D_IntegrationTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 
 // @AutoConfigureTestDatabase
-@TestPropertySource( locations = "D_IntegrationTest.properties")
+@TestPropertySource( locations = "application-integrationtest.properties")
 public class D_IntegrationTest {
 
     @LocalServerPort
